d0a39998d0d949959d67ee5b9eaba9d35c73d565
Dimitrios Tosidis <dimtosidis@gmail.com>
2023-03-30 00:07:43 +0300

    Update main.tf

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_app_service" "dimitrios" {
   name                = "udacity-dimitrios-azure-dotnet-app"
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  app_service_plan_id = azurerm_app_service_plan.dimitrios.id
 
   site_config {
     dotnet_framework_version = "v4.0"
