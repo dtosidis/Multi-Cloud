12e72462280d2bd7f411accfe79323f23b69e191
Dimitrios Tosidis <dimtosidis@gmail.com>
2023-03-29 23:33:42 +0300

    DNS name

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -7,17 +7,17 @@ resource "azurerm_container_group" "udacity" {
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
   ip_address_type     = "Public"
-  dns_name_label      = "udacity-tscotto-azure"
+  dns_name_label      = "udacity-dimitrios-azure"
   os_type             = "Linux"
 
   container {
     name   = "azure-container-app"
-    image  = "docker.io/tscotto5/azure_app:1.0"
+    image  = "docker.io/dimitrios/azure_app:1.0"
     cpu    = "0.5"
     memory = "1.5"
     environment_variables = {
-      "AWS_S3_BUCKET"       = "udacity-tscotto-aws-s3-bucket",
-      "AWS_DYNAMO_INSTANCE" = "udacity-tscotto-aws-dynamodb"
+      "AWS_S3_BUCKET"       = "udacity-dimitrios-aws-s3-bucket",
+      "AWS_DYNAMO_INSTANCE" = "udacity-dimitrios-aws-dynamodb"
     }
     ports {
       port     = 3000
